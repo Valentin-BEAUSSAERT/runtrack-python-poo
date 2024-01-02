294b055fa6c1e0d255ef3dc064ceca2b3bdf823d
SK4 <valentin.beaussaert@laplateforme.io>
2024-01-02 09:29:54 +0100

    Job1 ended

---
+++ job02.py
@@ -0,0 +1,8 @@
+class Operation:
+
+    def __init__(self, number1, number2):
+        self.number1 = 12
+        self.number2 = 3
+        
+operation = Operation()
+print()
---
+++ jour01/job01.py
@@ -0,0 +1,8 @@
+class Operation:
+
+    def __init__(self):
+        self.number1 = 0
+        self.number2 = 0
+        
+operation = Operation()
+print(operation)
